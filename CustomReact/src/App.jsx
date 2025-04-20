be69e98fd084700b32ccc5173d29474a3ae2ee4c
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-04-20 10:39:41 +0530

    react

--- CustomReact/src/App.jsx
+++ CustomReact/src/App.jsx
@@ -1,22 +1,36 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+let [counter, setCounter] = useState(15)
+// let counter = 15
+
+const addValue = () => {
+ // counter = counter + 1
+  setCounter(counter) 
+}
+
+const removeValue = () => {
+  setCounter(counter - 1)
+}
 
   return (
-    <>
+    <>  
    <h1>Chai aur react</h1> 
-   <h2>Counter value: 5</h2>
+   <h2>Counter value: {counter}</h2>
 
-   <button>Add Value</button>
-   <br />
-   <button>remove value</button>
-
+   <button onClick={removeValue}>
+   Add value</button>
+   <br/>
+   <button>removevalue</button>
+<p>
+  footer: {counter}
+</p>
     </>
-  )
+  ) 
 }
 
 export default App
