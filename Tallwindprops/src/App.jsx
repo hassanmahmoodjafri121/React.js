dd40abd280ef34fd6b27fb8e66aaee6e6559e0b0
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-04-22 21:19:30 +0530

    react

--- Tallwindprops/src/App.jsx
+++ Tallwindprops/src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  let myObj = {
+    name: 'Hitesh',
+    age: 25, 
+    address: {
+      city: 'Jaipur',
+      state: 'Rajasthan',
+      country: 'India'
+    }
+  }
+  let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-3xl bg-green-500 p-3 rounded-md' >Vite with Tailwind</h1>
+      <Card username="hitesh" myArr={newArr} />
+      <Card username='Json' post='Staff Engg.' />
+      <Card />
     </>
   )
 }
