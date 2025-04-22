201aa62554fdf02be281310271342878f6de7898
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-04-22 22:12:17 +0530

    react

--- Tallwindprops/src/App.jsx
+++ Tallwindprops/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 
@@ -8,7 +9,7 @@ function App() {
 
   let myObj = {
     name: 'Hitesh',
-    age: 25, 
+    age: 25,
     address: {
       city: 'Jaipur',
       state: 'Rajasthan',
